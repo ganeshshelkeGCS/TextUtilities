3dd571aa59ade0051950adb950b2cfc46e17708c
Ganesh shelke <ganeshshelke785@gmail.com>
2024-05-07 11:27:02 +0530

    initial commit

---
+++ src/components/TextForm.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+
+
+
+export default function TextForm(props) {
+
+    const handleUpClick = () => {
+        let newText = text.toUpperCase();
+        setText(newText);
+        props.showAlert("Converted to Uppercase", "success");
+    }
+
+    const handleLowClick = () => {
+        let newText = text.toLowerCase();
+        setText(newText);
+        props.showAlert("Converted to LowerCase", "success");
+    }
+
+    const handleClearClick = () => {
+        setText("");
+        props.showAlert("Text Cleared", "success");
+    }
+
+    const handleCopyClick = () => {
+        let text = document.getElementById("myBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+        props.showAlert("Text Copied to Clipboard!", "success");
+    }
+
+    const handleExtraSpaces = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "));
+        props.showAlert("Extra spaces Removed", "success");
+    }
+
+    const handleOnChange = (event) => {
+        console.log("On Change");
+        setText(event.target.value);
+    }
+
+    const [text, setText] = useState('');
+    // text = "new text"; // Wrong way to change the state. Correct way is use the setText() function
+
+  return (
+    <>
+    <div className="container" style={{color: props.mode === 'dark'?'white':'#042743'}}>
+        <h1 >{props.heading}</h1>
+      <div className="mb-3">
+        <textarea
+          className="form-control"
+          id="myBox"
+          rows="8"
+          value={text}
+          onChange={handleOnChange}
+          style={{backgroundColor: props.mode === 'dark'?'gray':'white'}}
+          >
+          </textarea>
+      </div>
+      <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+      <button className="btn btn-primary mx-2" onClick={handleLowClick}>Convert to Lowercase</button>
+      <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear Text</button>
+      <button className="btn btn-primary mx-2" onClick={handleCopyClick}>Copy Text</button>
+      <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>Remove Spaces</button>
+    </div>
+
+    <div className="container my-3" style={{color: props.mode === 'dark'?'white':'#042743', color: props.mode === 'dark'?'white':'#042743'}}>
+        <h2>Your text summary</h2>
+        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{0.08 * (text.split(" ").length)} Minutes read</p>
+        <h2>Preview</h2>
+        <p>{text .length > 0? text : "Enter something...."}</p>
+    </div>
+    </>
+  );
+}
---
+++ src/module2.mjs
@@ -0,0 +1,12 @@
+
+const a = "Harry";
+const b = "Rohan";
+const c = "Akash";
+const d = "Priyanka";
+const f = ["Rohan", "Akash", "Harry"];
+
+export default a;
+export {b};
+export {c};
+export {d};
+
